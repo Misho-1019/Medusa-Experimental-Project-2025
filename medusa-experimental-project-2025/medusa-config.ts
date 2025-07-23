59b480765462b4b69f4f82071989c17e9b6a2557
Misho1019 <mikailtorres99@gmail.com>
2025-07-23 03:23:40 +0300

    Install and set up Medusa

---
+++ medusa-experimental-project-2025/medusa-config.ts
@@ -0,0 +1,36 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  modules: [
+    {
+      resolve: "@medusajs/auth",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/auth-emailpass",
+            id: "emailpass",
+            options: {
+              hashConfig: {
+                logN: 15,
+                r: 8,
+                p: 1,
+              },
+            },
+          },
+        ],
+      },
+    },
+  ],
+})
